7cbc4237bff06152a6eedc2adaee429d06e04111
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-04 20:01:46 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,5 @@
-# Write your code here
-count = 0 
-while count < 3 do
-  puts "I am the #{count}, I love to count!"
-  count = count + 1 
+count = 0 # A bit of data defined outside the loop
+while count < 3 do # A Boolean expression using the bit of data
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1 # A bit of work that moves the bit of data closer to being false
 end
